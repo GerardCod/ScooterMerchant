3163e643ed4c1a6c5163f7a9a19f2bc89bba19bd
GerardCode <39347487+GerardCod@users.noreply.github.com>
2020-08-23 21:13:58 -0500

    Bug fix at Main.dart (#2)
    * Adding constants to project
    
    * Fonts added
    
    * Login design finished
    
    * Creation of BLoC pattern
    
    * Form validations finished
    
    * MerchantModel added
    
    * Adding SharedPreferences
    
    * Login successful
    
    * CustomAppBar added

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:scootermerchant/src/widgets/appbar_widget.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -6,8 +8,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
+      appBar: CustomAppBar(
+        title: 'SCOOTER',
       ),
       body: Center(
         child: Text('Home page'),
---
+++ lib/src/widgets/appbar_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:scootermerchant/utilities/constants.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  const CustomAppBar({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        this.title,
+        style: textStyleForAppBar,
+      ),
+      actions: <Widget>[
+        Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(60.0);
+}
