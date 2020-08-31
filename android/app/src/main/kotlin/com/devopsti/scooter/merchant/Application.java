9cc9945ec8b8cb42ee0f465aa961eec74589d638
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2020-08-31 15:17:48 -0500

    Push notifications implemented

---
+++ android/app/src/main/kotlin/com/devopsti/scooter/merchant/Application.java
@@ -0,0 +1,54 @@
+package com.devopsti.scooter.merchant;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+
+// Nota, esta linea la agregue
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
+
+public class Application extends FlutterApplication implements PluginRegistrantCallback {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    System.out.println("Notification sent");
+    FlutterFirebaseMessagingService.setPluginRegistrant(this);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel channel = new NotificationChannel("locations", "Locations", NotificationManager.IMPORTANCE_HIGH);
+      channel.setSound(null, null);
+      NotificationManager manager = getSystemService(NotificationManager.class);
+      manager.createNotificationChannel(channel);
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel channel2 = new NotificationChannel("alarms", "Alarms", NotificationManager.IMPORTANCE_HIGH);
+      // channel2.setSound(null, null);
+
+      Uri uri = Uri.parse("android.resource://" + this.getPackageName() + "/" + R.raw.ringtone);
+
+      AudioAttributes att = new AudioAttributes.Builder()
+              .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+              .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+              .build();
+      channel2.setSound(uri, att);
+      channel2.enableVibration(true);
+      channel2.enableLights(true);
+
+      NotificationManager manager = getSystemService(NotificationManager.class);
+      manager.createNotificationChannel(channel2);
+    }
+  }
+
+  @Override
+  public void registerWith(PluginRegistry registry) {
+    // GeneratedPluginRegistrant.registerWith(registry);
+    FirebaseMessagingPlugin.registerWith(registry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+  }
+}
--- android/app/src/main/kotlin/com/devopsti/scooter/merchant/MainActivity.kt
+++ android/app/src/main/kotlin/com/devopsti/scooter/merchant/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.devopsti.scooter.merchant
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.app.NotificationCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.PluginRegistry
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity() {}
+
