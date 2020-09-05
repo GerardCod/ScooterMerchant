572a88c28b326795088a9741ecaa009f17c79637
Jesus230899 <jesus.aguilar.e.229@gmail.com>
2020-09-05 18:45:04 -0500

    Cahnges in ProductDetails

--- lib/src/pages/product_details/product_details_page.dart
+++ lib/src/pages/product_details/product_details_page.dart
@@ -13,6 +13,10 @@ class ProductDetailsPage extends StatelessWidget {
     final ProductBlocProvider bloc = Provider.productBlocProviderOf(context);
     final Product model = ModalRoute.of(context).settings.arguments;
     final Size size = MediaQuery.of(context).size;
+    // bloc.changeProductName(model.name);
+    print('model.isAvailable======================');
+    print(model.isAvailable);
+    // bloc.changeProductPrice(model.price);
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -92,37 +96,37 @@ class ProductDetailsPage extends StatelessWidget {
       stream: bloc.productNameStream,
       builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
         return TextFormField(
-          initialValue: product.name,
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            contentPadding: EdgeInsets.all(16.0),
-            border: OutlineInputBorder(),
-            labelText: 'Nombre',
-            errorText: snapshot.error,
-          ),
-          onChanged: bloc.changeProductName,
-        );
+            initialValue: product.name,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(16.0),
+              border: OutlineInputBorder(),
+              labelText: 'Nombre',
+              errorText: snapshot.error,
+            ),
+            onChanged: bloc.changeProductName);
       },
     );
   }
 
   Widget _productPriceStreamBuilder(ProductBlocProvider bloc, Product product) {
     return StreamBuilder(
-        stream: bloc.productPriceStream,
-        builder: (BuildContext context, AsyncSnapshot<double> snapshot) {
-          return TextFormField(
-            initialValue: product.price.toString(),
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(16.0),
-                border: OutlineInputBorder(),
-                labelText: 'Precio',
-                errorText: snapshot.error),
-            onChanged: (String value) {
-              bloc.changeProductPrice(double.parse(value));
-            },
-          );
-        });
+      stream: bloc.productPriceStream,
+      builder: (BuildContext context, AsyncSnapshot<double> snapshot) {
+        return TextFormField(
+          initialValue: product.price.toString(),
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(16.0),
+              border: OutlineInputBorder(),
+              labelText: 'Precio',
+              errorText: snapshot.error),
+          onChanged: (value) {
+            bloc.changeProductPrice(double.parse(value));
+          },
+        );
+      },
+    );
   }
 
   Widget _switchDisponibility(ProductBlocProvider bloc, Product product) {
@@ -141,18 +145,20 @@ class ProductDetailsPage extends StatelessWidget {
         SizedBox(
           width: 8.0,
         ),
-        _switchStreamBuilder(bloc),
+        _switchStreamBuilder(bloc, product),
       ],
     );
   }
 
-  Widget _switchStreamBuilder(ProductBlocProvider bloc) {
+  Widget _switchStreamBuilder(ProductBlocProvider bloc, Product product) {
     return StreamBuilder(
       stream: bloc.productAvailableStream,
       builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
         // print(MerchantPreferences().isOpen);
         return Switch(
-            value: snapshot.hasData ? snapshot.data : false,
+            value: !snapshot.hasData  && snapshot.data == null
+                ? product.isAvailable
+                : snapshot.data,
             activeColor: colorSuccess,
             onChanged: (bool value) {
               bloc.changeProductAvailable(value);
@@ -164,38 +170,32 @@ class ProductDetailsPage extends StatelessWidget {
 
   Widget _buttonStreamBuilder(
       ProductBlocProvider bloc, Product product, BuildContext context) {
-    return StreamBuilder(
+    return StreamBuilder<bool>(
         stream: bloc.showLoaderStream,
         builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
           return Container(
             width: 200,
             child: RaisedButton(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Align(
-                      alignment: Alignment.center,
-                      child: Text('Guardar datos', style: textStyleBtnComprar)),
-                  Visibility(
-                    visible: snapshot.hasData && snapshot.data == true,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 10),
-                      child: SizedBox(
-                        height: 20,
-                        width: 20,
-                        child: CircularProgressIndicator(),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Guardar datos', style: textStyleBtnComprar),
+                    Visibility(
+                      visible: snapshot.hasData && snapshot.data == true,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(),
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-              shape: radiusButtons,
-              color: primaryColor,
-              onPressed:
-                  bloc.productName.length != 0 && bloc.productPrice != null
-                      ? () => this._updateProduct(product, bloc, context)
-                      : null,
-            ),
+                    )
+                  ],
+                ),
+                shape: radiusButtons,
+                color: primaryColor,
+                onPressed: () => this._updateProduct(product, bloc, context)),
           );
         });
   }
