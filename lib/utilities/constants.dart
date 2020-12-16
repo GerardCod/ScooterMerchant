74a0630ea9e3b154a64a9531848b69e32f71fffc
Jesus230899 <jesus.aguilar.e.229@gmail.com>
2020-12-15 22:00:27 -0600

    Changes in URL

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -6,11 +6,11 @@ String fontFamily = 'poppins';
 String fontFamilySecondary = 'Slant';
 String location = "America/Mexico_City";
 
-String baseUrl = 'https://www.scooterdev.tech/api/v1/';
-String baseUri = "www.scooterdev.tech";
+// String baseUrl = 'https://www.scooterdev.tech/api/v1/';
+// String baseUri = "www.scooterdev.tech";
 
-// String baseUrl = "https://api.lospedidos.site/api/v1/";
-// String baseUri = "api.lospedidos.site";
+String baseUrl = "https://api.lospedidos.site/api/v1/";
+String baseUri = "api.lospedidos.site";
 
 // final Map<String, int> status = {
 //   'incoming': 14,
